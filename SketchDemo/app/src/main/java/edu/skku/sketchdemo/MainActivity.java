b9be9463d05df9533fdbe3e9159f5ac2d5657fcd
HaewonCho <sala0320@naver.com>
2021-08-24 11:36:26 +0900

    refactoring communicate with server code

--- SketchDemo/app/src/main/java/edu/skku/sketchdemo/MainActivity.java
+++ SketchDemo/app/src/main/java/edu/skku/sketchdemo/MainActivity.java
@@ -5,13 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.res.Resources;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.icu.text.SimpleDateFormat;
+import android.icu.util.Output;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -30,6 +36,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 
@@ -38,7 +45,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import edu.skku.sketchdemo.ml.EffiExtractor;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.MultipartBody;
@@ -46,7 +52,6 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
-import smile.TSNE;
 
 public class MainActivity extends AppCompatActivity {
     private final int GET_IMAGE_FOR_GALLERYVIEW = 201;
@@ -58,7 +63,9 @@ public class MainActivity extends AppCompatActivity {
     private Button suggestButton;
     private Bitmap galleryImageBmp;
     private Classifier classifier;
-    File tempSelectFile;
+    private Uri selectedImageUri;
+    private File tempSelectFile;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -85,25 +92,14 @@ public class MainActivity extends AppCompatActivity {
             @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
             public void onClick(View view) {
-                Drawable galleryImageDrawable = galleryImage.getDrawable();
-                galleryImageBmp = ((BitmapDrawable)galleryImageDrawable).getBitmap();
-                //Bitmap galleryImageBmpResized = Bitmap.createScaledBitmap(galleryImageBmp, 224, 224, false);
-                String date = new SimpleDateFormat("yyyy_MM_dd_hh_mm_ss").format(new Date());
-                try {
-                    tempSelectFile = new File(Environment.getExternalStorageDirectory()+ "/Pictures/Test/", "temp_" + date + ".jpg");
-                    OutputStream out = new FileOutputStream(tempSelectFile);
-                    galleryImageBmp.compress(Bitmap.CompressFormat.JPEG, 100, out);
-                    goSend(tempSelectFile);
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
+                File sendImage = new File(getPath(selectedImageUri));
+                goSend(sendImage);
+
             }
         });
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        Uri selectedImageUri;
-        RequestOptions option1 = new RequestOptions().centerInside();
 
         super.onActivityResult(requestCode, resultCode, data);
 
@@ -123,13 +119,15 @@ public class MainActivity extends AppCompatActivity {
         }
     }
     public void goSend(File file){
+        Log.d("TEST : ", "Request");
+
         RequestBody requestBody = new MultipartBody.Builder()
                 .setType(MultipartBody.FORM)
-                .addFormDataPart("files", file.getName(), RequestBody.create(MultipartBody.FORM, file))
+                .addFormDataPart("files", "galleryfile", RequestBody.create(MultipartBody.FORM, file))
                 .build();
 
         Request request = new Request.Builder()
-                .url("http://192.168.43.221:18080/upload")
+                .url("http://blee.iptime.org:22222/haewon")
                 .post(requestBody)
                 .build();
 
@@ -143,28 +141,43 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onResponse(Call call, Response response) throws IOException {
+                Log.d("TEST : ", "Response");
                 String responseData = response.body().string();
                 Log.d("TEST : ", responseData);
                 try {
                     JSONObject json = new JSONObject(responseData);
+                    System.out.println(json);
+
                     List<String> suggestedImageList = new ArrayList<String>();
                     for (int i = 0; i < 4; i++) {
-                        suggestedImageList.add(i, json.getString("image" + Integer.toString(i)));
+                        suggestedImageList.add(i, json.getString("img" + (i + 1)));
                     }
-                    Resources resources = MainActivity.this.getResources();
-                    int resourceId1 = resources.getIdentifier(suggestedImageList.get(1), "drawable", MainActivity.this.getPackageName());
-                    int resourceId2 = resources.getIdentifier(suggestedImageList.get(2), "drawable", MainActivity.this.getPackageName());
-                    int resourceId3 = resources.getIdentifier(suggestedImageList.get(3), "drawable", MainActivity.this.getPackageName());
-                    int resourceId4 = resources.getIdentifier(suggestedImageList.get(4), "drawable", MainActivity.this.getPackageName());
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            Resources resources = MainActivity.this.getResources();
+                            int resourceId1 = resources.getIdentifier(suggestedImageList.get(0), "drawable", MainActivity.this.getPackageName());
+                            int resourceId2 = resources.getIdentifier(suggestedImageList.get(1), "drawable", MainActivity.this.getPackageName());
+                            int resourceId3 = resources.getIdentifier(suggestedImageList.get(2), "drawable", MainActivity.this.getPackageName());
+                            int resourceId4 = resources.getIdentifier(suggestedImageList.get(3), "drawable", MainActivity.this.getPackageName());
 
-                    suggestImage1.setImageResource(resourceId1);
-                    suggestImage2.setImageResource(resourceId2);
-                    suggestImage3.setImageResource(resourceId3);
-                    suggestImage4.setImageResource(resourceId4);
-                } catch (JSONException e) {
+                            suggestImage1.setImageResource(resourceId1);
+                            suggestImage2.setImageResource(resourceId2);
+                            suggestImage3.setImageResource(resourceId3);
+                            suggestImage4.setImageResource(resourceId4);
+                        }
+                    });
+                    } catch(JSONException e){
                     e.printStackTrace();
                 }
             }
         });
+    }
+    public String getPath(Uri uri) {
+        String[] projection = {MediaStore.Images.Media.DATA};
+        Cursor cursor = managedQuery(uri, projection, null, null, null);
+        startManagingCursor(cursor);
+        int columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(columnIndex);
     }
 }
